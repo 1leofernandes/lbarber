cc7e105398a96d5acdf2a839a59ab70942c77779
unknown <leonardoff24@gmail.com>
2026-04-09 11:08:47 -0300

    corrigindo a rota

--- src/routes/servicos.js
+++ src/routes/servicos.js
@@ -3,13 +3,15 @@ const express = require('express');
 const router = express.Router();
 const servicoController = require('../controllers/servicoController');
 
+
+router.get('/clear-cache', servicoController.clearCache);
+
 // GET /servicos - Buscar todos os serviços (público)
 router.get('/', servicoController.getAll);
+
 
 // GET /servicos/:id - Buscar serviço por ID (público)
 router.get('/:id', servicoController.getById);
 
-// ADICIONE ESTA LINHA
-router.get('/clear-cache', servicoController.clearCache);
 
 module.exports = router;
